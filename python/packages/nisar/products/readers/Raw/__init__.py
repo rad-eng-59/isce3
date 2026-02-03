1080837a05b23eef9e4950bfbc9619a0ff1802c7
Hirad Ghaemi <hirad.ghaemi@jpl.nasa.gov>
2026-02-03 00:16:54 -0800

    Augment Raw w/ QP-support chirp correlator and caltype functions

--- python/packages/nisar/products/readers/Raw/__init__.py
+++ python/packages/nisar/products/readers/Raw/__init__.py
@@ -1,2 +1,10 @@
-from .Raw import Raw, open_rrsd
+from .Raw import (
+    Raw,
+    open_rrsd,
+    chirpcorrelator_caltype_from_raw,
+    PolarizationTypeId,
+    is_raw_quad_pol,
+    first_tx_pol_for_quad,
+    opposite_pol
+)
 from .DataDecoder import complex32, DataDecoder
